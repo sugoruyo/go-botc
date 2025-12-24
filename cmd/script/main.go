f780c7a1fbc95073511704db9860247f5a15c9b3
George Vasilakakos <sugoruyo@gmail.com>
2025-12-25 00:03:12 +0200

    update script tester

--- cmd/script/main.go
+++ cmd/script/main.go
@@ -11,33 +11,49 @@ import (
 )
 
 func main() {
-	path := os.Args[1]
-	data, err := os.ReadFile(path)
+	scriptPath := os.Args[1]
+	scriptData, err := os.ReadFile(scriptPath)
 	if err != nil {
-		log.Fatalf("failed to read %s: %s", path, err)
+		log.Fatalf("failed to read %s: %s", scriptPath, err)
 	}
 	var s botc.Script
-	err = json.Unmarshal(data, &s)
+	err = json.Unmarshal(scriptData, &s)
 	if err != nil {
 		log.Fatalf("failed to unmarshal json: %s", err)
 	}
+	rosterPath := os.Args[2]
+	rosterData, err := os.ReadFile(rosterPath)
+	if err != nil {
+		log.Fatalf("failed to read %s: %s", rosterPath, err)
+	}
+	var r botc.Roster
+	err = json.Unmarshal(rosterData, &r)
+	if err != nil {
+		log.Fatalf("failed to unmarshal json: %s", err)
+	}
+
 	fmt.Printf("Script: %s by %s\n", s.Meta.Name, s.Author())
 	if s.Meta.Almanac != "" {
 		fmt.Printf("Learn more at %s\n", s.Meta.Almanac)
 	}
+	s.PopulateIndex(r)
 	fmt.Println("First Night Order:")
-	for i, j := range s.Meta.FirstNight {
-		fmt.Printf("%02d. %s\n", i+1, j)
+	for i, j := range s.FirstNight(r.Characters) {
+		fmt.Printf("%02d. %s\n", i+1, j.GetName())
 	}
 	fmt.Println("Other Night Order:")
-	for i, j := range s.Meta.OtherNight {
-		fmt.Printf("%02d. %s\n", i+1, j)
+	for i, j := range s.OtherNights(r.Characters) {
+		fmt.Printf("%02d. %s\n", i+1, j.GetName())
 	}
 	fmt.Println("Characters")
-	fmt.Printf("Original: %s\n", strings.Join(s.OriginalCharacterIds, ", "))
-	customs := make([]string, len(s.CustomCharacters))
-	for i, c := range s.CustomCharacters {
-		customs[i] = c.Name
+	if len(s.OriginalCharacterIds) > 0 {
+		fmt.Printf("Original: %s\n", strings.Join(s.OriginalCharacterIds, ", "))
 	}
-	fmt.Printf("Custom: %s\n", strings.Join(customs, ", "))
+	if len(s.CustomCharacters) > 0 {
+		customs := make([]string, len(s.CustomCharacters))
+		for i, c := range s.CustomCharacters {
+			customs[i] = c.Name
+		}
+		fmt.Printf("Custom: %s\n", strings.Join(customs, ", "))
+	}
 }
